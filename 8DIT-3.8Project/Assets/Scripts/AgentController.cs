f6af7996d3e8afab0fc5d3bd52d9fc3e6706ec14
AleksCheifetz <50680554+AleksCheifetz@users.noreply.github.com>
2020-06-17 13:53:31 +1200

    Very Basic Simulation Working
    I created a test building with 3 exits. I also wrote a script which spawns in a set number of agents which each pathfind outside the building. Next steps are to render the building properly in Blender, and get the evacuated agents to assemble in a grid

---
+++ 8DIT-3.8Project/Assets/Scripts/AgentController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform start = agent.GetComponent<Transform>();
+        Vector3 end = start.position;
+        end.x -= 50;
+
+        agent.SetDestination(end);
+    }
+}
---
+++ 8DIT-3.8Project/Assets/Scripts/SimulationManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationManager : MonoBehaviour
+{
+    public GameObject agentPrefab;
+    public int numOfAgents = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numOfAgents; i++)
+        {
+            int x = Random.Range(-40, 0);
+            int z = Random.Range(-40, 0);
+
+            GameObject agent = Instantiate(agentPrefab, new Vector3(x, 1, z), new Quaternion(0, 0, 0, 0));
+        }
+    }
+}
