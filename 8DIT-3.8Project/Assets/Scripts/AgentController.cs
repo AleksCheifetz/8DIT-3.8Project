10461e5e59114abe1cabc746e14cc23895465eef
AleksCheifetz <50680554+AleksCheifetz@users.noreply.github.com>
2020-06-18 18:57:59 +1200

    Small Improvements
    I've edited the spawning script so it no longer spawns agents in random positions. Instead it creates a grid which is completely flexible for the number of agents. As they spawn in a grid, they also assemble once evacuated in a grid.

--- 8DIT-3.8Project/Assets/Scripts/AgentController.cs
+++ 8DIT-3.8Project/Assets/Scripts/AgentController.cs
@@ -6,13 +6,16 @@ using UnityEngine.AI;
 public class AgentController : MonoBehaviour
 {
     public NavMeshAgent agent;
+    public SimulationManager simulationManager;
 
     // Start is called before the first frame update
     void Start()
     {
+        SimulationManager manager = simulationManager.GetComponent<SimulationManager>();
+
         Transform start = agent.GetComponent<Transform>();
         Vector3 end = start.position;
-        end.x -= 50;
+        end.x -= manager.sizeOfBuilding;
 
         agent.SetDestination(end);
     }
--- 8DIT-3.8Project/Assets/Scripts/SimulationManager.cs
+++ 8DIT-3.8Project/Assets/Scripts/SimulationManager.cs
@@ -7,15 +7,29 @@ public class SimulationManager : MonoBehaviour
     public GameObject agentPrefab;
     public int numOfAgents = 50;
 
+    public int sizeOfBuilding = 50;
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < numOfAgents; i++)
-        {
-            int x = Random.Range(-40, 0);
-            int z = Random.Range(-40, 0);
+        float limit = Mathf.Ceil(Mathf.Sqrt(numOfAgents));
+        int counter = 0;
 
-            GameObject agent = Instantiate(agentPrefab, new Vector3(x, 1, z), new Quaternion(0, 0, 0, 0));
+
+        for (int r = 0; r < limit; r++)
+        {
+            for (int c = 0; c < limit; c++)
+            {
+                if (counter < numOfAgents)
+                {
+                    float x = -((sizeOfBuilding / limit) + (sizeOfBuilding / limit * r));
+                    float z = -((sizeOfBuilding / limit) + (sizeOfBuilding / limit * c));
+
+                    GameObject agent = Instantiate(agentPrefab, new Vector3(x, 1, z), new Quaternion(0, 0, 0, 0));
+
+                    counter++;
+                }
+            }
         }
     }
 }
