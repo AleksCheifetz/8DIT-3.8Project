3179dffb16c7519376aeb60f16ff791f8a5485d0
AleksCheifetz <50680554+AleksCheifetz@users.noreply.github.com>
2020-08-08 11:32:24 +1200

    User Controlled Camera
    I've created a new script that moves the camera around based on the players WASD, Space, Shift, and mouse inputs. The only probelms currently are the movements are relative to the player, not the global position, and the movement is sped up with the time scale

---
+++ 8DIT-3.8Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 20;
+
+    public float speedH = 5.0f;
+    public float speedV = 5.0f;
+
+    private float yaw = -90.0f;
+    private float pitch = 0.0f;
+
+    bool paused = true;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+
+        if (paused)
+        {
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.visible = false;
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.AddRelativeForce(Vector3.forward * forwardForce);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.AddRelativeForce(Vector3.left * forwardForce);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.AddRelativeForce(Vector3.back * forwardForce);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.AddRelativeForce(Vector3.right * forwardForce);
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb.AddRelativeForce(Vector3.up * forwardForce);
+            }
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                rb.AddRelativeForce(Vector3.down * forwardForce);
+            }
+
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+    }
+}
